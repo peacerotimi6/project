7822de2cd0252ec1fdcaffc5ce875b5118d834fe
Peace Rotimi <peacerotimi6@gmail.com>
2025-12-23 17:20:02 +0000

    Add GitHub Actions CI pipeline

---
+++ app/test_app.py
@@ -0,0 +1,11 @@
+from main import add, multiply
+def test_add():
+    assert add(2, 3) == 5
+
+from main import subtract
+def test_subtract():
+    assert subtract(5, 3) == 2
+from main import multiply
+def test_multiply():
+    assert multiply(5, 3) == 15
+    
